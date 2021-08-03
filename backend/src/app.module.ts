f234a557dba9878f59d8f6b4d6a2a1cda8d98cdd
Abhishek Singh <abhishek.singh@olx.com>
2021-08-03 10:31:46 +0530

    configure typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
+import config from './../ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(config),
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
